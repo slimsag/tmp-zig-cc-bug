5c2334358d9ae66ea8ca20358adbe375a5cfddf7
Stephen Gutekanst <stephen@hexops.com>
2024-01-28 13:32:36 -0700

    reprorud
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/main.zig
+++ src/main.zig
@@ -1,29 +1,8 @@
 const std = @import("std");
 const glfw = @import("mach-glfw");
 
-/// Default GLFW error handling callback
-fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
-    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
-}
-
 pub fn main() !void {
-    glfw.setErrorCallback(errorCallback);
-    if (!glfw.init(.{})) {
-        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
-        std.process.exit(1);
-    }
-    defer glfw.terminate();
-
-    // Create our window
-    const window = glfw.Window.create(640, 480, "Hello, mach-glfw!", null, null, .{}) orelse {
-        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
-        std.process.exit(1);
-    };
-    defer window.destroy();
-
-    // Wait for the user to close the window.
-    while (!window.shouldClose()) {
-        window.swapBuffers();
-        glfw.pollEvents();
-    }
+    const glfw_native = glfw.Native(.{ .cocoa = true });
+    const ns_window = glfw_native.getCocoaWindow(.{ .handle = undefined });
+    _ = ns_window;
 }
